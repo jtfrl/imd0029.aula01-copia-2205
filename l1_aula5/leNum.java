abccaa412fd9bf893ad927ca7f4e3bf40f3a5ba6
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-30 23:58:24 -0300

    Update leNum.java
    add leNum ajustado

--- l1_aula5/leNum.java
+++ l1_aula5/leNum.java
@@ -1,34 +1,29 @@
 import java.util.Scanner; //importação para ler números
 
-
 public class leNum{
-	public static void main(String[] args) {
-	int[] v={1,8,9,17,26};
-	
-	Scanner s= new Scanner(System.in);
-	System.out.println("Digite um número: ");
-	int n=s.nextInt();
+    public static void main(String[] args) {
+        int[] v={1,8,9,17,26};
 
-    Boolean c=true;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n=s.nextInt();
 
-	for(int i=0;i<=v.length;i++){
-	    if(v[i]==n){
-	        c=true;
-	        break;
-	    }else{
-	        c=false;
-	        break;
-	    }
-	    
-	}
-	
-	if(c==true) {
-	    System.out.println("O número "+n+" consta no array!");
-	    
-	}else{
-	    System.out.println("O número "+n+" não consta no array!");
-	}
-	
-    s.close(); //fecha o scanner
-	}
+        Boolean c=false;
+
+        for(int i=0;i<v.length;i++){
+            if(v[i]==n){
+                c=true;
+                break; //sai assim que encontra
+            }
+        }
+
+        if(c) {
+            System.out.println("O número "+n+" consta no array!");
+
+        }else{
+            System.out.println("O número "+n+" não consta no array!");
+        }
+
+        s.close(); //fecha o scanner
+    }
 }
