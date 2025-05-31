4c39556153e7b8275053cf5851984eaa30f2846b
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-05-31 00:39:23 -0300

    Update leNumPoSt.java
    bool add

--- l1_aula5/leNumPoSt.java
+++ l1_aula5/leNumPoSt.java
@@ -8,17 +8,23 @@ public class leNumPoSt{
         System.out.println("Digite um número: ");
         int n=s.nextInt();
         int pos=0;
+        Boolean c=false;
 
         for(int i=0;i<t.length;i++){
             if(t[i]==n){
                 pos=i;
+                c=true;
                 break;
             }
         }
 
+        if(c){
         System.out.println("O número "+n+" está na posição "+pos+" (menor posição entre todas as ocorrências).");
-
-
+        }
+        else{
+        System.out.println(-1);
+        }
+        
     s.close();
     }
 }
